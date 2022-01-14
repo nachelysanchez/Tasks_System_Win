c767eb7f6d12516464ab367188368bcb071e7476
nachelysanchez <nashely-5@hotmail.com>
2022-01-14 16:44:37 -0400

    Agregando migración

---
+++ Migrations/20220114204421_Migracion_Inicial.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Tasks_System_Win.Migrations
+{
+    public partial class Migracion_Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tareas",
+                columns: table => new
+                {
+                    TareaId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nombre = table.Column<string>(type: "TEXT", nullable: true),
+                    Descripcion = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tareas", x => x.TareaId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Tareas",
+                columns: new[] { "TareaId", "Descripcion", "Nombre" },
+                values: new object[] { 1, "La persona X debe de analizar el area que su superior le asigno.", "Análisis" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tareas");
+        }
+    }
+}
