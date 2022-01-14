9513d215547b8ae69de9d4f6d3aad15e3b2deee7
nachelysanchez <nashely-5@hotmail.com>
2022-01-14 16:39:42 -0400

    Creando la entidad Tareas

---
+++ Entidades/Tareas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks_System_Win.Entidades
+{
+    public class Tareas
+    {
+        [Key]
+        public int TareaId { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
