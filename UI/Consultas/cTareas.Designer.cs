64698376753234a114eab786eb0af67b2b37fc85
nachelysanchez <nashely-5@hotmail.com>
2022-01-14 18:56:44 -0400

    Diseñando y programando la consulta de tareas

---
+++ UI/Consultas/cTareas.Designer.cs
@@ -0,0 +1,168 @@
+﻿
+namespace Tasks_System_Win.UI.Consultas
+{
+    partial class cTareas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FiltroComboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.CriterioTextBox = new System.Windows.Forms.TextBox();
+            this.BuscarButton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TareaID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // FiltroComboBox
+            // 
+            this.FiltroComboBox.FormattingEnabled = true;
+            this.FiltroComboBox.Items.AddRange(new object[] {
+            "Todos",
+            "Id",
+            "Nombre",
+            "Descripción"});
+            this.FiltroComboBox.Location = new System.Drawing.Point(12, 27);
+            this.FiltroComboBox.Name = "FiltroComboBox";
+            this.FiltroComboBox.Size = new System.Drawing.Size(163, 23);
+            this.FiltroComboBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Filtro";
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Location = new System.Drawing.Point(197, 9);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(46, 15);
+            this.Label2.TabIndex = 2;
+            this.Label2.Text = "Criterio";
+            // 
+            // CriterioTextBox
+            // 
+            this.CriterioTextBox.Location = new System.Drawing.Point(197, 27);
+            this.CriterioTextBox.Name = "CriterioTextBox";
+            this.CriterioTextBox.Size = new System.Drawing.Size(196, 23);
+            this.CriterioTextBox.TabIndex = 3;
+            // 
+            // BuscarButton
+            // 
+            this.BuscarButton.Location = new System.Drawing.Point(410, 27);
+            this.BuscarButton.Name = "BuscarButton";
+            this.BuscarButton.Size = new System.Drawing.Size(75, 23);
+            this.BuscarButton.TabIndex = 4;
+            this.BuscarButton.Text = "Buscar";
+            this.BuscarButton.UseVisualStyleBackColor = true;
+            this.BuscarButton.Click += new System.EventHandler(this.BuscarButton_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 77);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(489, 271);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Id";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 50;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Nombre";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 150;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Descripción";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 250;
+            // 
+            // TareaID
+            // 
+            this.TareaID.HeaderText = "Tarea Id";
+            this.TareaID.Name = "TareaID";
+            this.TareaID.ReadOnly = true;
+            // 
+            // cTareas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(513, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.BuscarButton);
+            this.Controls.Add(this.CriterioTextBox);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.FiltroComboBox);
+            this.Name = "cTareas";
+            this.Text = "cTareas";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox FiltroComboBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Label2;
+        private System.Windows.Forms.TextBox CriterioTextBox;
+        private System.Windows.Forms.Button BuscarButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TareaID;
+    }
+}
---
+++ UI/Consultas/cTareas.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tasks_System_Win.BLL;
+using Tasks_System_Win.Entidades;
+
+namespace Tasks_System_Win.UI.Consultas
+{
+    public partial class cTareas : Form
+    {
+        
+        public cTareas()
+        {
+            InitializeComponent();
+        }
+
+        private void BuscarButton_Click(object sender, EventArgs e)
+        {
+            List<Tareas> listado = new List<Tareas>();
+            string criterio = CriterioTextBox.Text.Trim();
+
+            if (CriterioTextBox.Text.Trim().Length > 0)
+            {
+                switch (FiltroComboBox.SelectedIndex)
+                {
+                    case 0: // Todos
+                        listado = TareasBLL.GetTareas();
+                        break;
+                    case 1: //Id
+                        listado = TareasBLL.GetList(t => t.TareaId == Utilidades.ToInt(CriterioTextBox.Text));
+                        break;
+                    case 2: //Nombres
+                        listado = TareasBLL.GetList(t => t.Nombre.ToLower().Contains(CriterioTextBox.Text.ToLower()));
+                        break;
+                    case 3: //Descripcion
+                        listado = TareasBLL.GetList(t => t.Descripcion.ToLower().Contains(CriterioTextBox.Text.ToLower()));
+                        break;
+                }
+            }
+            else
+            {
+                listado = TareasBLL.GetList(t => true);
+            }
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = listado;
+        }
+    }
+}
