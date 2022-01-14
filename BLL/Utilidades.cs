1698304d5c360c85e60ef9fd5b2e5b96c64d7e9d
nachelysanchez <nashely-5@hotmail.com>
2022-01-14 17:57:52 -0400

    Creando la clase Utilidades

---
+++ BLL/Utilidades.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks_System_Win.BLL
+{
+    public class Utilidades
+    {
+        public static int ToInt(string valor)
+        {
+            int retorno;
+
+            int.TryParse(valor, out retorno);
+
+            return retorno;
+        }
+    }
+}
